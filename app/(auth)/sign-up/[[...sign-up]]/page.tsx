b9d8802ac8ee1866d0232f6153095e35970929ed
Davi Mateus <davialmeida@atomstudio.com.br>
2024-05-28 10:12:08 -0300

    fix: text

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -7,7 +7,7 @@ const SignUpPage = () => {
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
       <div className="h-full lg:flex flex-col items-center justify-center px-4">
         <div className="text-center space-y-4 pt-16">
-          <h1 className="font-bold text-3xl text-[#2E2A47]">Bem vindo!</h1>
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Bem vindo(a)!</h1>
           <p className="text-base text-[#7E8CA0]">
             Cadastre-se para começar a usar o Financify e gerenciar suas
             finanças!
--- components/welcome-message.tsx
+++ components/welcome-message.tsx
@@ -8,7 +8,7 @@ export const WelcomeMessage = () => {
   return (
     <div className="space-y-2 mb-4">
       <h2 className="text-2xl lg:text-4xl text-white font-medium">
-        Bem-vindo de volta{isLoaded ? ", " : " "}
+        Bem-vindo(a) de volta{isLoaded ? ", " : " "}
         {user?.firstName}👋
       </h2>
       <p className="text-sm lg:text-base text-[#89B6FD]">
